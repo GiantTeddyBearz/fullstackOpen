abc3bd66fd1a321345ae172ba02a6b257c4a1e92
GiantTeddyBear <giantteddybear1@gmail.com>
2020-12-25 00:50:43 -0800

    2.5, courseinfo separate Course module

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,42 +1,45 @@
 import React from 'react';
 
+const Course = ({course}) =>(
+  <>
+    <Header course={course} />
+    <Content course={course} />
+    <Total course={course} />
+  </>
+  
+)
 const Header = ({ course }) => {
-    return (
-      <h1>{course.name}</h1>
-    )
-  }
-  
-  const Total = ({ course }) => {
-    const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
-    return(
-      <p>Number of exercises {sum}</p>
-    ) 
-  }
-  
-  const Part = (props) => {
-    return (
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>    
-    )
-  }
-  
-  const Content = ({ course }) => {
-    return (
-      <div>
-        <Part part={course.parts[0]} />
-        <Part part={course.parts[1]} />
-        <Part part={course.parts[2]} />
-      </div>
-    )
-  }
-  
-const Course = ({course}) =>{
-    <>
-        <Header course={course} />
-        <Content course={course} />
-        <Total course={course} />
-    </>
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue.exercises
+  const sum = course.parts.reduce(reducer, 0);
+
+  return(
+    <p><b>total of {sum} exercises</b></p>
+  ) 
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ course }) => {
+  const copy = course.parts.map((value) => 
+    <Part key={value.id} name={value.name} exercises={value.exercises}/>
+  )
+  return (
+    <div>
+      {copy}
+    </div>
+  )
 }
 
 export default Course
--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,47 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Course = ({course}) =>(
-  <>
-    <Header course={course} />
-    <Content course={course} />
-    <Total course={course} />
-  </>
-  
-)
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Total = ({ course }) => {
-  const reducer = (accumulator, currentValue) => accumulator + currentValue.exercises
-  const sum = course.parts.reduce(reducer, 0);
-
-  return(
-    <p><b>total of {sum} exercises</b></p>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  const copy = course.parts.map((value) => 
-    <Part key={value.id} name={value.name} exercises={value.exercises}/>
-  )
-  return (
-    <div>
-      {copy}
-    </div>
-  )
-}
+import Course from './components/Course.js'
 
 const App = () => {
   const courses = [
