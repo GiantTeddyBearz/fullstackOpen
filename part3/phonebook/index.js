eb55fb5a87a5bbd757ce9819f4f4068dcb3a1ea0
GiantTeddyBearz <giantteddybear1@gmail.com>
2021-01-11 12:51:43 -0800

    3.6 phonebook backend step6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -77,6 +77,18 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
+    if (!body.number){
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
     const person = {
         id: CreateNewId(),
         name: body.name,
