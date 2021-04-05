1c5dd7a5fab25a98fca8932a1261559e2c9fa280
GiantTeddyBear <giantteddybear1@gmail.com>
2021-04-04 17:59:07 -0700

    redo and refresh on part 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,105 +1,82 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
+const express = require('express')
+const app = express()
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1> Hello World!</h1>');
-})
-app.get('/api/persons', (request, response) => {
-    response.json(persons);
-})
+app.use(express.json())
 
-app.get('/info', (request, response) => {
-    const size = persons.length;
-    const date = new Date();
-    response.send(`
-    <div>
-    <p> Phonebook has info for ${size} people </p>
-    </div>
-    <div>
-        ${date}
-    </div>
-    `)
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(person => person.id === id);
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end();
-    }
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(person => person.id !== id);
-
-    response.status(204).end();
+//single resource
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
 })
 
-const CreateNewId = () => {
-    return Math.floor(Math.random() * 100000);
-}
+//deleting resources
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter( note => note.id !== id)
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body;
-
-    console.log(body)
-    if (!body.name){
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
-
-    if (!body.number){
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
-
-    if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const person = {
-        id: CreateNewId(),
-        name: body.name,
-        number: body.number
-    }
-
-    persons = persons.concat(person);
-    response.json(person);
+  res.status(204).end()
 })
 const PORT = 3001
-
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
+  console.log(`Server running on port ${PORT}`)
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+  console.log (...notes.map(n => n.id))
+  return maxId + 1
+}
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'content missing'
+    })
+  }
+  
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  }
+
+  notes = notes.concat(note)
+
+  res.json(note)
 })
